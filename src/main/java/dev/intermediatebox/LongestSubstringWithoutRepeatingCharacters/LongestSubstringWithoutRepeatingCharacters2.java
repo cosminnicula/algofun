6802c9fffd22d5469c89f975fd5dfc739905318d
Cosmin Nicula <582616+cosminnicula@users.noreply.github.com>
2022-05-27 14:01:59 +0300

    Add two numbers, Longest substring without repeating characters

---
+++ src/main/java/dev/intermediatebox/LongestSubstringWithoutRepeatingCharacters/LongestSubstringWithoutRepeatingCharacters2.java
@@ -0,0 +1,35 @@
+package dev.intermediatebox.LongestSubstringWithoutRepeatingCharacters;
+
+import java.util.HashSet;
+import java.util.Set;
+
+// pwwkew -> longest substring is wke
+// abcabcbb -> longest substring is abc
+
+// time complexity O(n^2), space complexity O(1)
+
+// if only ASCII characters, then Set could be replaced with bool[256]
+// Unicode supports 1,114,112 characters, while Java int has a maximum value of 2^32-1, so a Set or a Map is more efficient
+public class LongestSubstringWithoutRepeatingCharacters2 {
+  public int run(String s) {
+    int maxLength = 0;
+    Set<Integer> visited;
+
+    for (int i = 0; i < s.length(); i++) {
+      visited = new HashSet<>();
+      for (int j = i; j < s.length(); j++) {
+        if (!areUnique(visited, Character.getNumericValue(s.charAt(j)))) {
+          break;
+        }
+        visited.add(Character.getNumericValue(s.charAt(j)));
+      }
+      maxLength = Math.max(maxLength, visited.size());
+    }
+
+    return maxLength;
+  }
+
+  private boolean areUnique(Set<Integer> visited, Integer currentChar) {
+    return !visited.contains(currentChar);
+  }
+}
---
+++ src/main/java/dev/intermediatebox/Main.java
@@ -0,0 +1,18 @@
+package dev.intermediatebox;
+
+import dev.intermediatebox.LongestSubstringWithoutRepeatingCharacters.LongestSubstringWithoutRepeatingCharacters1;
+import dev.intermediatebox.LongestSubstringWithoutRepeatingCharacters.LongestSubstringWithoutRepeatingCharacters2;
+
+public class Main {
+  public static void main(String[] args) {
+//    AddTwoNumbers1 atn = new AddTwoNumbers1();
+//
+//    List<Integer> list1 = List.of(3, 5, 7);
+//    List<Integer> list2 = List.of(8, 7);
+//
+//    System.out.println(atn.run(list1, list2));
+
+    LongestSubstringWithoutRepeatingCharacters2 l = new LongestSubstringWithoutRepeatingCharacters2();
+    System.out.println(l.run("pwwkew"));
+  }
+}
