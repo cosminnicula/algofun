7b8c651c19d2b6c40948669b484426b6635d7a50
Cosmin Nicula <582616+cosminnicula@users.noreply.github.com>
2022-06-02 23:07:08 +0300

    Binary tree node class

---
+++ src/main/java/dev/intermediatebox/utils/Node.java
@@ -0,0 +1,11 @@
+package dev.intermediatebox.utils;
+
+public class Node {
+  public int value;
+  public Node left;
+  public Node right;
+
+  public Node(int value) {
+    this.value = value;
+  }
+}
